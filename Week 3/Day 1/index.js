addca9112a4aa8e35cf1970ea98f4bc08de8bbeb
WE9D.YY <41113180+w99dyy@users.noreply.github.com>
2025-10-27 00:20:25 +0300

    Add files via upload

---
+++ Week 3/Day 1/index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+
+let countEL = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEL.textContent = count
+
+
+}
+
+function save() {
+    let countStr = count + " / "
+    saveEl.textContent += countStr
+        countEL.textContent = 0
+        count = 0
+}
+
+
